794e1908fbe452604b7f30153afa6315396c7630
sjoerdpouwels <sjoerdpouwels@gmail.com>
2025-09-26 16:24:58 +0200

    Add files via upload

---
+++ cloud/cloud.js
@@ -0,0 +1,33 @@
+{
+    "version": "1.7.3",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 215774,
+    "boundingBox": {
+        "lx": 31.96697919368744,
+        "ly": 3.893942505121231,
+        "lz": 18.101818237304689,
+        "ux": 33.811979222297669,
+        "uy": 5.738942533731461,
+        "uz": 19.946818265914918
+    },
+    "tightBoundingBox": {
+        "lx": 31.96697919368744,
+        "ly": 3.893942505121231,
+        "lz": 18.101818237304689,
+        "ux": 32.78997919368744,
+        "uy": 4.686942505121231,
+        "uz": 19.946818237304688
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 0.015978168696165086,
+    "scale": 0.001,
+    "hierarchyStepSize": 5,
+    "pointClassCount": {
+        "0": 215774
+    }
+}
